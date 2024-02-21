f569c9a57f315e20739dae479c5036fc89310f81
mujtabaAli96 <48852703+mujtabaAli96@users.noreply.github.com>
2024-02-21 17:03:54 +0500

    new

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename="/krossit">
+      <Routes>
+
+        <Route  path="/" element={<App />} /> 
+
+
+      </Routes>
+    </BrowserRouter>
+)
