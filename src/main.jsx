65f65543fcd908775f3b8899c307be95f65077e8
mujtabaAli96 <48852703+mujtabaAli96@users.noreply.github.com>
2024-02-26 00:25:00 +0500

    latest

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/krossit">
+  <BrowserRouter basename="/">
       <Routes>
 
         <Route  path="/" element={<App />} /> 
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lawgical.pk/krossit",
+  base: "https://krossit.vercel.app/",
 
 })
